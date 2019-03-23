4027d5a33bfff5b16f1657e0d3f9a0d6b460dda0
Pedro <kroip333@gmail.com>
2019-03-23 16:14:52 +0000

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+    this.connecToServer = this.connecToServer.bind(this);
+  }
+
+  connecToServer() {
+    fetch('/');
+  }
+
+  componentDidMount() {
+    this.connecToServer();
+  }
+  
   render() {
     return (
       <div className="App">
