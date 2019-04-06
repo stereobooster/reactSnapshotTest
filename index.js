c2aa09991bf982c0ae132944863a53e498b9ec52
Pedro <kroip333@gmail.com>
2019-04-06 21:06:28 +0100

    fix bug

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ if(process.env.NODE_ENV === 'production') {
   //
   app.get('*', (req, res) => {
 
-    res.sendFile(path.join(__dirname = 'client/build'+req.path+'.html'));
+    res.sendFile(path.join(__dirname + 'client/build'+req.path+'.html'));
   })
 }
 
