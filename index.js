f12177bf872c7895e255a5825f0f4f8bcfe55731
Pedro <kroip333@gmail.com>
2019-04-06 21:11:09 +0100

    bug fix 2

--- index.js
+++ index.js
@@ -65,11 +65,10 @@ app.get('/somestaticpage', (req, res) => {
 
 //production mode
 if(process.env.NODE_ENV === 'production') {
-  // app.use(express.static(path.join(__dirname, 'client/build')));
-  //
+  
   app.get('*', (req, res) => {
 
-    res.sendFile(path.join(__dirname + 'client/build'+req.path+'.html'));
+    res.sendFile(path.join(__dirname + '/client/build'+req.path+'.html'));
   })
 }
 
